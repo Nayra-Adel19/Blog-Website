03021802634fb6203b7526851a5c72667752411e
Nayra-Adel19 <nayra.adel19@email.com>
2024-10-13 22:07:32 +0200

    feat: Implement Create-blog route with validation, JWT authentication, and Cloudinary integration
    - Added '/create-blog' POST route to handle blog creation.
    - Included JWT verification middleware for authentication.
    - Integrated Cloudinary for banner image handling.
    - Implemented validation for blog title, description, banner, content, and tags.
    - Saved blog to MongoDB with automatic author association and unique blog ID generation.
    - Updated user's total posts on successful blog creation.

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import cors from "cors";
 import admin from "firebase-admin";
 import { v2 as cloudinary } from 'cloudinary';
 import { readFile } from 'fs/promises';
+import Blog from './Schema/Blog.js';
 
 // Load service account key from JSON file
 const serviceAccountKey = JSON.parse(await readFile(new URL('./react-js-duck-blog-firebase-adminsdk-jq9ct-0380317e4a.json',
@@ -53,6 +54,26 @@ server.post("/upload", async(req, res) => {
         res.status(500).json({ error: 'Failed to upload image' });
     }
 });
+
+const verifyJWT = (req, res, next) => {
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) {
+        return res.status(401).json({ error: 'No access token' });
+    }
+
+    jwt.verify(token, process.env.SECRET_ACCESS_KEY, (err, user) => {
+
+        if (err) {
+            return res.status(403).json({ error: 'Access token is invalid' });
+        }
+
+        req.user = user.id
+        next()
+    })
+}
 
 const formatDatatoSend = (user) => {
     const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY);
@@ -164,6 +185,69 @@ server.post("/google-auth", async(req, res) => {
         return res.status(500).json({ "error": "Failed to authenticate you with google. Try with some other google account" });
     }
 });
+
+// Create Blog
+server.post('/create-blog', verifyJWT, (req, res) => {
+
+    let authorId = req.user;
+    let { title, des, banner, tags, content, draft } = req.body;
+
+    if (!title.length) {
+        return res.status(403).json({ error: "You must provide a title to publish the blog" });
+    }
+
+    if (!des.length || des.length > 200) {
+        return res.status(403).json({ error: "You must provide blog description under 200 characters" });
+    }
+
+    if (!banner.length) {
+        return res.status(403).json({ error: "You must provide blog banner to publish it" });
+    }
+
+    if (!content.blocks.length) {
+        return res.status(403).json({ error: "There must be some blog content to publish it" });
+    }
+
+    if (!tags.length || tags.length > 10) {
+        return res.status(403).json({ error: "Provide tags in order to publish the blog, Maximum 10" });
+    }
+
+    tags = tags.map(tag => tag.toLowerCase());
+
+    let blog_id = title.replace(/[^a-zA-Z0-9]/g, '').replace(/\s+/g, "-").trim() + nanoid();
+
+    let blog = new Blog({
+        title,
+        des,
+        banner,
+        content,
+        tags,
+        author: authorId,
+        blog_id,
+        draft: Boolean(draft)
+    })
+
+    blog.save().then(blog => {
+
+        let incrementVal = draft ? 0 : 1;
+
+        user.findOneAndUpdate({ _id: authorId }, { $inc: { "account_info.total_posts": incrementVal }, $push: { "blogs": blog._id } })
+            .then(User => {
+                return res.status(200).json({ id: blog.blog_id })
+            })
+            .catch(err => {
+                return res.status(500).json({ error: "Failed to update total posts number" })
+            })
+
+    })
+
+    .catch(err => {
+        return res.status(500).json({ error: err.message })
+    })
+
+    //return res.json({ status: "Done" });
+
+})
 
 server.listen(PORT, () => {
     console.log('Listening on port -> ' + PORT);
