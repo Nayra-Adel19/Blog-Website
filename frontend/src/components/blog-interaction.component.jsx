67f44fd209277efbda9d05a9b4b9b18b8e82872e
Bassant-Adel <baadel404@gmail.com>
2024-10-18 01:12:29 +0300

    feat: Implement comments section with CommentField and CommentsContainer
    - Added CommentField component for user input.
    - Created CommentsContainer to manage comment display and interaction.
    - Integrated context for managing blog title and comments visibility.
    - Implemented basic styling and layout for the comments section.

--- frontend/src/components/blog-interaction.component.jsx
+++ frontend/src/components/blog-interaction.component.jsx
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../App";
 import { Toaster, toast } from "react-hot-toast";
 import { useEffect } from "react";
+import axios from 'axios';
 
 const BlogInteraction = () => {
 
   let { blog, blog: { _id, title, blog_id, activity, activity: { total_likes, total_comments }, author: {
     personal_info: { username: author_username }
-  }}, setBlog, isLikedByUser, setLikedByUser } = useContext(BlogContext);
+  }}, setBlog, isLikedByUser, setLikedByUser, setCommentsWrapper } = useContext(BlogContext);
 
   let { userAuth: { username, access_token } } = useContext(UserContext);
 
@@ -71,7 +72,7 @@ useEffect(() => {
           </button>
           <p className="text-xl text-dark-grey">{ total_likes }</p>
 
-          <button className="w-10 h-10 rounded-full flex items-center justify-center bg-grey/80">
+          <button onClick={() => setCommentsWrapper(preVal => !preVal)} className="w-10 h-10 rounded-full flex items-center justify-center bg-grey/80">
             <i className="fi fi-rr-comment-dots"></i>
           </button>
           <p className="text-xl text-dark-grey">{ total_comments }</p>
