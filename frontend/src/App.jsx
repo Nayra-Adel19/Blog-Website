2f17632c0c0004d4f41ec361b885b8db4712f2a4
Bassant-Adel <baadel404@gmail.com>
2024-10-17 03:48:18 +0200

    feat: Enhance Blog Editor and Publishing Features in frontend side
    - Updated the main application structure to improve user authentication and blog management.
    - Integrated context for user authentication, enabling session handling and token management.
    - Enhanced the Editor component to support both blog editing and publishing functionalities.
    - Implemented loading states for better user experience during data fetching and processing.
    - Improved the PublishForm component to enforce validations on title, description, and tags before publishing.
    - Added dynamic rendering of blog content using the BlogEditor, ensuring a seamless writing experience.
    - Introduced error handling and feedback mechanisms using toast notifications for user actions.
    
    These updates provide a more robust and user-friendly interface for managing blog content, enhancing overall application performance and usability.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Editor from "./pages/editor.pages";
 import SearchPage from "./pages/search.page";
 import ProfilePage from "./pages/profile.page";
 import PageNotFound from "./pages/404.page";
+import BlogPage from "./pages/blog.page";
 
 
 export const UserContext = createContext({})
@@ -31,12 +32,14 @@ const App = () => {
     <UserContext.Provider value={{ userAuth, setUserAuth }}>
       <Routes>
 				<Route path="/editor" element={<Editor />} />
+				<Route path="/editor/:blog_id" element={<Editor />} />
         <Route path="/" element={<Navbar />}>
 					<Route index element={<HomePage />} />
           <Route path="signin" element={<UserAuthForm type="sign-in" />} />
           <Route path="signup" element={<UserAuthForm type="sign-up" />} />
 					<Route path="search/:query" element={<SearchPage />} />
 					<Route path="user/:id" element={<ProfilePage />} />
+					<Route path="blog/:blog_id" element={<BlogPage/>} />
 					<Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
