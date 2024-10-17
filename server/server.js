b547943aa1c58fe0856b2c9cbcf0585e12d666fe
Nayra-Adel19 <nayra.adel19@gmail.com>
2024-10-17 04:05:37 +0300

    feat: Update API endpoint for creating, retrieving, and searching blogs

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import cors from "cors";
 import admin from "firebase-admin";
 import { v2 as cloudinary } from 'cloudinary';
 import { readFile } from 'fs/promises';
-import Blog from './Schema/Blog.js';
 
 // Load service account key from JSON file
 const serviceAccountKey = JSON.parse(await readFile(new URL('./react-js-duck-blog-firebase-adminsdk-jq9ct-0380317e4a.json',
@@ -16,6 +15,7 @@ const serviceAccountKey = JSON.parse(await readFile(new URL('./react-js-duck-blo
 
 import { getAuth } from "firebase-admin/auth";
 import user from './Schema/User.js';
+import Blog from './Schema/Blog.js';
 
 const server = express();
 const PORT = 3000;
@@ -264,19 +264,19 @@ server.get('/trending-blogs', (req, res) => {
 // Search Blogs
 server.post("/search-blogs", (req, res) => {
 
-    let { tag, query, author, page } = req.body;
+    let { tag, query, author, page, limit, eliminate_blog } = req.body;
 
     let findQuery;
 
     if (tag) {
-        findQuery = { tags: tag, draft: false };
+        findQuery = { tags: tag, draft: false, blog_id: { $ne: eliminate_blog } };
     } else if (query) {
         findQuery = { draft: false, title: new RegExp(query, 'i') };
     } else if (author) {
         findQuery = { author, draft: false };
     }
 
-    let maxLimit = 2;
+    let maxLimit = limit ? limit : 2;
 
     Blog.find(findQuery)
         .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
@@ -326,7 +326,7 @@ server.post('/get-profile', (req, res) => {
 server.post('/create-blog', verifyJWT, (req, res) => {
 
     let authorId = req.user;
-    let { title, des, banner, tags, content, draft } = req.body;
+    let { title, des, banner, tags, content, draft, id } = req.body;
 
     if (!title.length) {
         return res.status(403).json({ error: "You must provide a title to publish the blog" });
@@ -354,44 +354,53 @@ server.post('/create-blog', verifyJWT, (req, res) => {
 
     tags = tags.map(tag => tag.toLowerCase());
 
-    let blog_id = title.replace(/[^a-zA-Z0-9]/g, '').replace(/\s+/g, "-").trim() + nanoid();
+    let blog_id = id || title.replace(/[^a-zA-Z0-9]/g, '').replace(/\s+/g, "-").trim() + nanoid();
 
-    let blog = new Blog({
-        title,
-        des,
-        banner,
-        content,
-        tags,
-        author: authorId,
-        blog_id,
-        draft: Boolean(draft)
-    })
-
-    blog.save().then(blog => {
-
-        let incrementVal = draft ? 0 : 1;
-
-        user.findOneAndUpdate({ _id: authorId }, { $inc: { "account_info.total_posts": incrementVal }, $push: { "blogs": blog._id } })
-            .then(User => {
-                return res.status(200).json({ id: blog.blog_id })
+    if (id) {
+        Blog.findOneAndUpdate({ blog_id }, { title, des, banner, content, tags, draft: draft ? draft : false })
+            .then(() => {
+                return res.status(200).json({ id: blog_id });
             })
             .catch(err => {
-                return res.status(500).json({ error: "Failed to update total posts number" })
+                return res.status(500).json({ error: err.message });
             })
+    } else {
+        let blog = new Blog({
+            title,
+            des,
+            banner,
+            content,
+            tags,
+            author: authorId,
+            blog_id,
+            draft: Boolean(draft)
+        })
 
-    })
+        blog.save().then(blog => {
 
-    .catch(err => {
-        return res.status(500).json({ error: err.message })
-    })
+            let incrementVal = draft ? 0 : 1;
 
-    //return res.json({ status: "Done" });
+            user.findOneAndUpdate({ _id: authorId }, { $inc: { "account_info.total_posts": incrementVal }, $push: { "blogs": blog._id } })
+                .then(User => {
+                    return res.status(200).json({ id: blog.blog_id })
+                })
+                .catch(err => {
+                    return res.status(500).json({ error: "Failed to update total posts number" })
+                })
 
+        })
+
+        .catch(err => {
+            return res.status(500).json({ error: err.message })
+        })
+
+        //return res.json({ status: "Done" });
+    }
 })
 
 server.post('/get-blog', (req, res) => {
-    let { blog_id } = req.body;
-    let incrementVal = 1;
+    let { blog_id, draft, mode } = req.body;
+    let incrementVal = mode !== 'edit' ? 1 : 0;
     Blog.findOneAndUpdate({ blog_id }, { $inc: { "activity.total_reads": incrementVal } })
         .populate("author", "personal_info.fullname personal_info.username personal_info.profile_img")
         .select("title des content banner activity publishedAt blog_id tags")
@@ -404,6 +413,12 @@ server.post('/get-blog', (req, res) => {
                         error: err.message
                     })
                 })
+
+            if (blog.draft && !draft) {
+                return res.status(500).json({
+                    error: err.message
+                });
+            }
             return res.status(200).json({ blog });
         })
         .catch(err => {
