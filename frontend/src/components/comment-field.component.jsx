aa89e475a9c1cff74619b9a96818ff97e9bdc3c5
Bassant-Adel <baadel404@gmail.com>
2024-10-22 02:49:37 +0300

    feat: implement CommentCard component for rendering blog comments
    - Introduced the `CommentCard` component to display individual comments, including the commenter's profile image, name, and the comment text.
    - The component destructures `commentData` to extract user information (`fullname`, `profile_img`, `username`) and the comment details (`commentedAt`, `comment`).
    - Added left indentation (`leftVal`) to visually represent the nesting level of comments.
    - Used `getDay` utility from the `../common/date` module to format the comment date (`commentedAt`).
    - Included styling for responsive layout, including spacing between elements and text truncation for long usernames.

--- frontend/src/components/comment-field.component.jsx
+++ frontend/src/components/comment-field.component.jsx
@@ -6,7 +6,9 @@ import axios from "axios";
 
 const CommentField = ({ action }) => {
 
-  let { blog, blog: { _id, author: { _id: blog_author }, comments, activity, activity: { total_comments, total_parent_comments } }, setBlog, setTotalParentCommentsLoaded } = useContext(BlogContext);
+  let { blog, blog: { _id, author: { _id: blog_author }, comments,
+  comments: { results: commentsArr },
+  activity, activity: { total_comments, total_parent_comments } }, setBlog, setTotalParentCommentsLoaded } = useContext(BlogContext);
   let { userAuth: { access_token, username, fullname, profile_img } } = useContext(UserContext);
   const [ comment, setComment ] = useState("");
 
@@ -34,7 +36,7 @@ const CommentField = ({ action }) => {
 
       data.childrenLevel = 0;
 
-      newCommentArr = [ data ];
+      newCommentArr = [ data, ...commentsArr ];
 
       let parentCommentIncrementVal = 1;
 
