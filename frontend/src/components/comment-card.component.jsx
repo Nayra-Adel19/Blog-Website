376416c4ba03416447a2d2dfa46600c31d232fc8
Bassant-Adel <baadel404@gmail.com>
2024-10-23 18:41:50 +0200

    feat(comments): add functionality to hide and load comment replies for frontend
    - Implemented the ability to load replies for individual comments dynamically.
    - Added support for hiding previously loaded replies to improve UX and reduce clutter.
    - Updated `CommentCard` to handle showing or hiding replies based on user interaction.
    - Integrated logic to fetch replies from the server and display them inline with appropriate nesting.
    - Improved comment rendering by managing children comment levels for better structure.

--- frontend/src/components/comment-card.component.jsx
+++ frontend/src/components/comment-card.component.jsx
@@ -1,19 +1,75 @@
 import { getDay } from "../common/date";
 import { useContext, useState } from "react";
 import { UserContext } from "../App";
+import { BlogContext } from "../pages/blog.page";
 import toast from "react-hot-toast";
 import CommentField from '../components/comment-field.component';
-
+import axios from "axios";
 
 const CommentCard = ({ index, leftVal, commentData }) => {
 
-  let { commented_by: { personal_info: { profile_img, fullname, username } }, commentedAt, comment, _id } = commentData;
+  let { commented_by: { personal_info: { profile_img, fullname, username } }, commentedAt, comment, _id, children = [] } = commentData;
+
+	let { blog, blog: { comments, comments: { results: commentsArr } }, setBlog } = useContext(BlogContext);
 
 	let { userAuth: { access_token } } = useContext(UserContext);
 
 	let [ isReplying, setReplying ] = useState(false)
 
+	const removeCommentsCards = (startingPoint) => {
+
+		if(commentsArr[startingPoint]){
+
+			while ( commentsArr[startingPoint].childrenLevel > commentData.childrenLevel ) {
+				commentsArr.splice(startingPoint, 1)
+
+				if(!commentsArr[startingPoint]){
+					break;
+				}
+			}
+		}
+
+		setBlog({ ...blog, comments: { results: commentsArr } })
+	}
+
 	//console.log("commentedAt = ", getDay(commentedAt))
+	// console.log("commentData.isReplyLoaded = ", commentData.isReplyLoaded)
+
+	console.log("children.length = ",children.length);
+
+	const loadReplies = ({ skip = 0 }) => {
+
+		if(children.length){
+
+			hideReplies();
+
+			axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-replies", { _id, skip })
+			.then(({ data: { replies } }) => {
+
+				commentData.isReplyLoaded = true;
+
+				for( let i = 0; i < replies.length; i++ ){
+					replies[i].childrenLevel = commentData.childrenLevel + 1;
+
+					commentsArr.splice(index + 1 + i + skip, 0, replies[i])
+				}
+
+				setBlog({ ...blog, comments: { ...comments, results: commentsArr } })
+
+			})
+			.catch(err => {
+				console.log(err);
+			})
+
+		}
+
+	}
+
+	const hideReplies = () => {
+		commentData.isReplyLoaded = false;
+
+		removeCommentsCards(index + 1)
+	}
 
 	const handleReplyClick = () => {
 		if(!access_token) {
@@ -36,6 +92,18 @@ const CommentCard = ({ index, leftVal, commentData }) => {
         <p className="font-gelasio text-xl ml-3">{comment}</p>
 
 				<div className="flex gap-5 items-center mt-5">
+
+					{
+						commentData.isReplyLoaded ?
+						<button className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2" onClick={hideReplies}>
+							<i className="fi fi-rs-comment-dots"></i> Hide Reply
+						</button> : (
+						<button className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2" onClick={loadReplies}>
+							<i className="fi fi-rs-comment-dots"></i> {children.length} Reply
+						</button>
+						)
+					}
+
 					<button className="underline" onClick={handleReplyClick}>Reply</button>
 				</div>
 
@@ -43,7 +111,7 @@ const CommentCard = ({ index, leftVal, commentData }) => {
 					isReplying ?
 					<div className="mt-8">
 						<CommentField action="reply" index={index} replyingTo={_id} setReplying={setReplying} />
-					</div> : ""
+					</div> : null
 				}
 
       </div>
